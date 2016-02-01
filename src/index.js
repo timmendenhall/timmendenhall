91fc1b5c1195ea308f4a9363478fc9ca9aa45b6a
Tim Mendenhall <timmendenhall@gmail.com>
2016-01-31 20:40:48 -0500

    More react-redux stuff

--- src/index.js
+++ src/index.js
@@ -3,13 +3,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router} from 'react-router'
 import { createHistory } from 'history';
+import { Provider } from 'react-redux';
+
+import configureStore from './redux/configureStore';
 import routes from './routes';
 
-var history = createHistory();
+const history = createHistory();
+const initialState = window.__INITIAL_STATE__;
+const store = configureStore(initialState);
 
 // Render the main component into the dom
 ReactDOM.render((
-    <Router history={history}>
-        {routes}
-    </Router>
+    <Provider store={store}>
+        <Router history={history}>
+            {routes}
+        </Router>
+    </Provider>
 ), document.getElementById('app'));
--- src/redux/middleware/logger.js
+++ src/redux/middleware/logger.js
@@ -1,4 +1,4 @@
-export const logger = store => next => action => {
+export const logger = (store) => (next) => (action) => {
     console.group(action.type);
     console.info('dispatching', action);
     let result = next(action);
