d71caf6b0c2ea3e8eb37bfe6dca59938fe16d8eb
Tim Mendenhall <timmendenhall@gmail.com>
2016-02-02 10:56:06 -0500

    adding sizes to columns and fixing blog column sizing

--- src/views/index/Index.js
+++ src/views/index/Index.js
@@ -1,17 +1,23 @@
 import React from 'react';
+import { Link } from 'react-router';
 import Container from '../../components/grid/Container';
 import Row from '../../components/grid/Row';
 import Column from '../../components/grid/Column';
+import Glyph from '../../components/Glyph';
 
 class HomeComponent extends React.Component {
     render() {
         return (
-            <div>
+            <div className="home">
                 <Container>
                     <Row>
-                        <Column span="8">
-                        </Column>
-                        <Column span="4">
+                        <Column span="12">
+                            <div className="jumbotron">
+                                <h1>Full Stack Developer</h1>
+                                <p className="lead">I'm a full stack web developer with a passion for technology.</p>
+                                <p className="lead">Catch up on how I'm using new frameworks and languages on my <Link to="/blog">blog</Link>.  Or view some of my past work:</p>
+                                <p><Link to="/work" className="btn btn-lg btn-success" role="button">View Example Work  <Glyph type="share-alt"/></Link></p>
+                            </div>
                         </Column>
                     </Row>
                 </Container>
